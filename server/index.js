4601ea1e344d5487dd0ea71227424be55b0da2c6
NayabMaryam <nayabmaryam40@gmail.com>
2026-08-17 21:34:35 +0500

    feat: added  project structure

---
+++ server/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()); // lets us read JSON data sent from the frontend
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
