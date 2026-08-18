7a35f50d1d24a2fb0a2fee94f1679d07012087e3
Software Engineering <baoalana123@gmail.com>
2026-08-18 13:40:37 +0500

    chore: integrate Stripe for checkout functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 import Navbar from './components/Navbar';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Products from './pages/Products';
 import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
 
 function App() {
   return (
@@ -14,6 +19,14 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Products />} />
         <Route path="/cart" element={<Cart />} />
+        <Route
+          path="/checkout"
+          element={
+            <Elements stripe={stripePromise}>
+              <Checkout />
+            </Elements>
+          }
+        />
       </Routes>
     </div>
   );
