1064fedcf16fbefd76528b7a0f283d76568f9a86
Software Engineering <baoalana123@gmail.com>
2026-08-18 13:38:35 +0500

    chore: add Cart route to App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Products from './pages/Products';
+import Cart from './pages/Cart';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </div>
   );
